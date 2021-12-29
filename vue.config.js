2fce69550b4b3934a131183e0626e47cf4175f82
Nicolas Sá de Souza <nsds26@hotmail.com>
2021-12-29 17:15:39 -0300

    Netlify Deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 //Configuring Proxy to handle the CORS error:
-module.exports = {
-	devServer: {
-		proxy: "https://api.atendare.com/v1/",
-	},
-};
+// module.exports = {
+// 	devServer: {
+// 		proxy: "https://api.atendare.com/v1/",
+// 	},
+// };
