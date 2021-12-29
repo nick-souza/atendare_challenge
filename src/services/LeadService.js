f5a544eb84241c9bb2108a546ca4490ab10a1eef
Nicolas Sá de Souza <nsds26@hotmail.com>
2021-12-29 17:37:14 -0300

    Final

--- src/services/LeadService.js
+++ src/services/LeadService.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 //Using LocalHost because I set the proxy in the vue.config.js to get rid of the CORS error:
-// const BASE_URL = "http://localhost:8080/lead";
-const BASE_URL = "https://atendare.netlify.app/lead";
+const BASE_URL = "http://localhost:8080/lead";
 
 //Token:
 const TOKEN = "fErAbPBLKVvl9IIL5Bk2ZE8X7tZ78xNKPm6MFi6orF0LqWgx85ou8QvBeZBmucMrhl3Sj2fUc2JUJTghNb8BJxk7W4HLHqyPYIDF";
